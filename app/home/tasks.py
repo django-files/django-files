4b76fa31dc9331feefa450d02d47a2e2835c68e1
Shane <6071159+smashedr@users.noreply.github.com>
2023-08-15 19:06:44 -0700

    API Caching

--- app/home/tasks.py
+++ app/home/tasks.py
@@ -36,7 +36,7 @@ def flush_template_cache():
 def clear_files_cache():
     # Clear Files cache
     log.info('clear_files_cache')
-    return cache.delete_pattern('template.cache.files*')
+    return cache.delete_pattern('*files*')
 
 
 @shared_task(autoretry_for=(Exception,), retry_kwargs={'max_retries': 3, 'countdown': 10})
