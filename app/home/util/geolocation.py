08a8b61270e590a62d3c4ab096d59499f54b5929
Shane <6071159+smashedr@users.noreply.github.com>
2023-08-16 01:05:50 -0700

    Minor Fixes

--- app/home/util/geolocation.py
+++ app/home/util/geolocation.py
@@ -11,8 +11,7 @@ def city_state_from_exif(gps_ifd: dict) -> str:
     # TODO: Possibly fix try/expect
     try:
         geolocator = Nominatim(user_agent='django-files')
-        lat_lon = dms_to_degrees(gps_ifd)
-        location = geolocator.reverse(lat_lon)
+        location = geolocator.reverse(dms_to_degrees(gps_ifd))
         if not (area := location.raw['address'].get('city')):
             area = location.raw['address'].get('county')
         state = location.raw['address'].get('state', '')
@@ -22,13 +21,9 @@ def city_state_from_exif(gps_ifd: dict) -> str:
         return ''
 
 
-def dms_to_degrees(gps_ifd: dict) -> Optional[Tuple[float, float]]:
-    # TODO: This should not be a try/expect, but upstream does not handle it
-    try:
-        dn, mn, sn = gps_ifd[2]
-        dw, mw, sw = gps_ifd[4]
-        lat_deg = dn + mn / 60 + sn / 3600
-        lon_deg = dw + mw / 60 + sw / 3600
-        return lat_deg, -lon_deg
-    except Exception as error:
-        log.info(error)
+def dms_to_degrees(gps_ifd: dict) -> Tuple[float, float]:
+    dn, mn, sn = gps_ifd[2]
+    dw, mw, sw = gps_ifd[4]
+    lat_deg = dn + mn / 60 + sn / 3600
+    lon_deg = dw + mw / 60 + sw / 3600
+    return lat_deg, -lon_deg
