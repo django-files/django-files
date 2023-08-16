b919ab9bcc4e8b739400e7ff60aa799bb7e68991
SmAsHeD <6071159+smashedr@users.noreply.github.com>
2023-08-15 19:28:32 -0700

    API Caching (#56)

--- app/api/urls.py
+++ app/api/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('', views.api_view, name='index'),
     path('recent/', views.recent_view, name='recent'),
     path('remote/', views.remote_view, name='remote'),
+    path('users/', views.users_view, name='users'),
 ]
