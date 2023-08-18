d02fbe9c4935225b14086b383ebfcedff58f842d
Shane <6071159+smashedr@users.noreply.github.com>
2023-08-17 20:34:00 -0700

    Update Style

--- app/static/main/js/login.js
+++ app/static/main/js/login.js
@@ -1,36 +1,37 @@
 // Document Dot Ready
-$(document).ready(function() {
-
+$(document).ready(function () {
     // Local login form handler
-    $('.form-control').focus(function() {
-        $(this).removeClass('is-invalid');
-    });
-    $('#login-form').on('submit', function(event){
-        event.preventDefault();
-        if ($('#login-button').hasClass('disabled')) { return; }
-        var formData = new FormData($(this)[0]);
+    $('.form-control').focus(function () {
+        $(this).removeClass('is-invalid')
+    })
+    $('#login-form').on('submit', function (event) {
+        event.preventDefault()
+        if ($('#login-button').hasClass('disabled')) {
+            return
+        }
+        let formData = new FormData($(this)[0])
         $.ajax({
             url: $('#login-form').attr('action'),
             type: 'POST',
             data: formData,
-            beforeSend: function(){
-                $('#login-button').addClass('disabled');
+            beforeSend: function () {
+                $('#login-button').addClass('disabled')
             },
-            complete: function(){
-                $('#login-button').removeClass('disabled');
+            complete: function () {
+                $('#login-button').removeClass('disabled')
             },
-            success: function(){
-                location.reload();
+            success: function () {
+                location.reload()
             },
-            error: function() {
-                $('#login-form input').addClass('is-invalid');
+            error: function () {
+                $('#login-form input').addClass('is-invalid')
             },
             cache: false,
             contentType: false,
-            processData: false
-        });
-        return false;
-    });
+            processData: false,
+        })
+        return false
+    })
 
     // var vid = document.getElementById("bgvid");
     // var pauseButton = document.querySelector("#pause");
@@ -52,5 +53,4 @@ $(document).ready(function() {
     //         pauseButton.innerHTML = "Play";
     //     }
     // })
-
-});
+})
--- app/static/main/js/main.js
+++ app/static/main/js/main.js
@@ -1,21 +1,23 @@
-$(document).ready(function() {
-
+$(document).ready(function () {
     // Back to Top Button, Function, and Listener
-    let mybutton = document.getElementById("btn-back-to-top");
+    let mybutton = document.getElementById('btn-back-to-top')
     window.onscroll = function () {
-      scrollFunction();
-    };
+        scrollFunction()
+    }
     function scrollFunction() {
-        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-            mybutton.style.display = "block";
+        if (
+            document.body.scrollTop > 20 ||
+            document.documentElement.scrollTop > 20
+        ) {
+            mybutton.style.display = 'block'
         } else {
-            mybutton.style.display = "none";
+            mybutton.style.display = 'none'
         }
     }
-    mybutton.addEventListener("click", backToTop);
+    mybutton.addEventListener('click', backToTop)
     function backToTop() {
-      document.body.scrollTop = 0;
-      document.documentElement.scrollTop = 0;
+        document.body.scrollTop = 0
+        document.documentElement.scrollTop = 0
     }
 
     // // Monitor home websockets for new data
@@ -27,45 +29,51 @@ $(document).ready(function() {
     // };
 
     // Init a ClipboardJS attribute
-    new ClipboardJS('.clip');
+    new ClipboardJS('.clip')
 
     // Onclick listener for clip
     $(document).ready(function () {
         $('.clip').click(function () {
-            var clipElement = $(this);
+            var clipElement = $(this)
             clipElement.popover({
                 content: 'Copied',
                 placement: 'bottom',
-                trigger: 'manual'
-            });
-            clipElement.popover('show');
+                trigger: 'manual',
+            })
+            clipElement.popover('show')
             setTimeout(function () {
-                clipElement.popover('hide');
-            }, 2000);
+                clipElement.popover('hide')
+            }, 2000)
             $(document).on('click', function (e) {
-                if (!clipElement.is(e.target) && clipElement.has(e.target).length === 0) {
-                    clipElement.popover('hide');
+                if (
+                    !clipElement.is(e.target) &&
+                    clipElement.has(e.target).length === 0
+                ) {
+                    clipElement.popover('hide')
                 }
-            });
-        });
-    });
+            })
+        })
+    })
 
     // Show custom toast-alert classes on load
-    $(".toast-alert").each(function() {
-        console.log($(this).html());
-        let toastAlert = new bootstrap.Toast($(this));
-        toastAlert.show();
-    });
-
-});
+    $('.toast-alert').each(function () {
+        console.log($(this).html())
+        let toastAlert = new bootstrap.Toast($(this))
+        toastAlert.show()
+    })
+})
 
 // Generate a BS toast and show it
 function show_toast(message, bsClass = 'info', delay = '5000') {
-  let toastContainer = $('.toast-container');
-  let toast = $('<div class="toast align-items-center border-0" role="alert" aria-live="assertive" aria-atomic="true" data-bs-delay="' + delay + '"><div class="d-flex"><div class="toast-body"></div><button type="button" class="btn-close me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button></div></div>');
-  toast.find('.toast-body').text(message);
-  toast.addClass('text-bg-' + bsClass);
-  toastContainer.append(toast);
-  let jsToast = new bootstrap.Toast(toast);
-  jsToast.show();
+    let toastContainer = $('.toast-container')
+    let toast = $(
+        '<div class="toast align-items-center border-0" role="alert" aria-live="assertive" aria-atomic="true" data-bs-delay="' +
+            delay +
+            '"><div class="d-flex"><div class="toast-body"></div><button type="button" class="btn-close me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button></div></div>'
+    )
+    toast.find('.toast-body').text(message)
+    toast.addClass('text-bg-' + bsClass)
+    toastContainer.append(toast)
+    let jsToast = new bootstrap.Toast(toast)
+    jsToast.show()
 }
