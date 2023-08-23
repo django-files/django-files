04ba745d778fe38ecaf56f404de9ea5727c6cd66
SmAsHeD <6071159+smashedr@users.noreply.github.com>
2023-08-22 19:50:47 -0700

    Update Style (#65)
    * Update Style
    
    * Fix Codacy Issues
    
    * Fix Codacy Issues
    
    * Fix Codacy Issues

--- app/static/main/js/files.js
+++ app/static/main/js/files.js
@@ -1,55 +1,55 @@
-$(document).ready(function() {
-
+$(document).ready(function () {
     // Get and set the csrf_token
-    const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
+    const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value
 
     // Define Hook Modal and Delete handlers
-    const deleteHookModal = new bootstrap.Modal('#delete-file-modal', {});
-    let hookID;
+    const deleteHookModal = new bootstrap.Modal('#delete-file-modal', {})
+    let hookID
     $('.delete-file-btn').click(function () {
-        hookID = $(this).data('hook-id');
-        console.log(hookID);
-        deleteHookModal.show();
-    });
+        hookID = $(this).data('hook-id')
+        console.log(hookID)
+        deleteHookModal.show()
+    })
 
     // Handle delete click confirmations
     $('#confirm-delete-hook-btn').click(function () {
-        if ($('#confirm-delete-hook-btn').hasClass('disabled')) { return; }
-        console.log(hookID);
+        if ($('#confirm-delete-hook-btn').hasClass('disabled')) {
+            return
+        }
+        console.log(hookID)
         $.ajax({
             type: 'POST',
             url: `/ajax/delete/file/${hookID}/`,
-            headers: {'X-CSRFToken': csrftoken},
+            headers: { 'X-CSRFToken': csrftoken },
             beforeSend: function () {
-                console.log('beforeSend');
-                $('#confirm-delete-hook-btn').addClass('disabled');
+                console.log('beforeSend')
+                $('#confirm-delete-hook-btn').addClass('disabled')
             },
             success: function (response) {
-                console.log('response: ' + response);
-                deleteHookModal.hide();
-                console.log('removing #file-' + hookID);
-                let count = $('#files-table tr').length;
-                $('#file-' +hookID).remove();
-                if (count<=2) {
-                    console.log('removing #files-table@ #files-table');
-                    $('#files-table').remove();
+                console.log('response: ' + response)
+                deleteHookModal.hide()
+                console.log('removing #file-' + hookID)
+                let count = $('#files-table tr').length
+                $('#file-' + hookID).remove()
+                if (count <= 2) {
+                    console.log('removing #files-table@ #files-table')
+                    $('#files-table').remove()
                 }
-                let message = 'File ' + hookID + ' Successfully Removed.';
-                show_toast(message,'success');
+                let message = 'File ' + hookID + ' Successfully Removed.'
+                show_toast(message, 'success')
             },
             error: function (xhr, status, error) {
-                console.log('xhr status: ' + xhr.status);
-                console.log('status: ' + status);
-                console.log('error: ' + error);
-                deleteHookModal.hide();
+                console.log('xhr status: ' + xhr.status)
+                console.log('status: ' + status)
+                console.log('error: ' + error)
+                deleteHookModal.hide()
                 let message = xhr.status + ': ' + error
-                show_toast(message,'danger', '15000');
+                show_toast(message, 'danger', '15000')
             },
             complete: function () {
-                console.log('complete');
-                $('#confirm-delete-hook-btn').removeClass('disabled');
-            }
-        });
-    });
-
-});
+                console.log('complete')
+                $('#confirm-delete-hook-btn').removeClass('disabled')
+            },
+        })
+    })
+})
--- app/static/main/js/home.js
+++ app/static/main/js/home.js
@@ -1,37 +1,36 @@
 $(document).ready(function () {
-
-    // Reload page on browser back/forward if new results
-    let perfEntries = performance.getEntriesByType('navigation');
-    if (perfEntries[0].type === 'back_forward') {
-        let reloadSession = localStorage.getItem('reloadSession');
-        if (reloadSession === 'true') {
-            localStorage.removeItem('reloadSession');
-            location.reload();
-        }
-    }
-
-    // Monitor websockets for new data and update results
-    const socket = new WebSocket('wss://' + window.location.host + '/ws/home/');
-    console.log('Websockets Connected.');
-    socket.onmessage = function (event) {
-        let data = JSON.parse(event.data);
-        $.get('/ajax/tdata/' + data.pk, function (response) {
-            $('#results tbody').prepend(response);
-            localStorage.setItem('reloadSession', 'true');
-            let message = 'New Test Result: ' + data.pk;
-            show_toast(message, 'success', '10000');
-            console.log('Table Updated: ' + data.pk);
-        });
-    };
+    // // Reload page on browser back/forward if new results
+    // let perfEntries = performance.getEntriesByType('navigation')
+    // if (perfEntries[0].type === 'back_forward') {
+    //     let reloadSession = localStorage.getItem('reloadSession')
+    //     if (reloadSession === 'true') {
+    //         localStorage.removeItem('reloadSession')
+    //         location.reload()
+    //     }
+    // }
+    //
+    // // Monitor websockets for new data and update results
+    // const socket = new WebSocket('wss://' + window.location.host + '/ws/home/')
+    // console.log('Websockets Connected.')
+    // socket.onmessage = function (event) {
+    //     let data = JSON.parse(event.data)
+    //     $.get('/ajax/tdata/' + data.pk, function (response) {
+    //         $('#results tbody').prepend(response)
+    //         localStorage.setItem('reloadSession', 'true')
+    //         let message = 'New Test Result: ' + data.pk
+    //         show_toast(message, 'success', '10000')
+    //         console.log('Table Updated: ' + data.pk)
+    //     })
+    // }
 
     // Get and set the csrf_token
-    const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
+    const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value
 
     // Generate Short URLs BUTTON
     // $('#create-short-btn').click(function () {
     $('#quick-short-form').on('submit', function (event) {
-        let data = {url: $("#long-url").val()};
-        event.preventDefault();
+        let data = { url: $('#long-url').val() }
+        event.preventDefault()
         // if (!data) {
         //     alert('Missing URL');
         // }
@@ -39,30 +38,39 @@ $(document).ready(function () {
             // url: $('#create-short-btn').attr('data-target-url'),
             url: $('#quick-short-form').attr('action'),
             type: 'POST',
-            headers: {'X-CSRFToken': csrftoken},
+            headers: { 'X-CSRFToken': csrftoken },
             // data: JSON.stringify(data),
             data: JSON.stringify(data),
             beforeSend: function (jqXHR) {
                 //
             },
             success: function (data, textStatus, jqXHR) {
-                console.log('Status: ' + jqXHR.status + ', Data: ' + JSON.stringify(data));
-                alert('Short Created: ' + data['url']);
-                location.reload();
+                console.log(
+                    'Status: ' +
+                        jqXHR.status +
+                        ', Data: ' +
+                        JSON.stringify(data)
+                )
+                alert('Short Created: ' + data['url'])
+                location.reload()
             },
             complete: function (data, textStatus) {
                 //
             },
             error: function (data, status, error) {
-                console.log('Status: ' + data.status + ', Response: ' + data.responseText);
+                console.log(
+                    'Status: ' +
+                        data.status +
+                        ', Response: ' +
+                        data.responseText
+                )
                 // alert(data.responseText);
                 let message = data.status + ': ' + data.responseJSON['error']
-                show_toast(message, 'danger', '6000');
+                show_toast(message, 'danger', '6000')
             },
             cache: false,
             contentType: false,
-            processData: false
-        });
-    });
-
-});
+            processData: false,
+        })
+    })
+})
