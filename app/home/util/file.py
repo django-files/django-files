496ea8e67a9acdbbfe2116f85f6f570e503a5e90
Ralph Luaces <ralph@luac.es>
2023-08-25 20:13:38 -0500

    preliminary fixes

--- app/home/util/file.py
+++ app/home/util/file.py
@@ -1,6 +1,7 @@
 import logging
 import mimetypes
 import os
+import tempfile
 # from django.conf import settings
 from django.core.files import File
 # from pathlib import Path
@@ -8,7 +9,6 @@ from typing import IO
 
 from home.models import Files
 from home.util.image import ImageProcessor
-from home.util.s3 import use_s3
 from home.tasks import send_discord_message
 from oauth.models import CustomUser
 
@@ -27,24 +27,28 @@ def process_file(name: str, f: IO, user_id: int, **kwargs) -> Files:
     log.info('process_file_upload: name: %s', name)
     user = CustomUser.objects.get(id=user_id)
     log.info('user: %s', user)
-    file = Files.objects.create(file=File(f, name=name), user=user, **kwargs)
-    file.name = os.path.basename(file.file.name)
-    log.info('file.name: %s', file.name)
-    file.mime, _ = mimetypes.guess_type(file.file.path, strict=False)
-    if not file.mime:
-        file.mime, _ = mimetypes.guess_type(file.file.name, strict=False)
-    file.mime = file.mime or 'application/octet-stream'
-    log.info('file.mime: %s', file.mime)
-    file.size = file.file.size
-    log.info('file.size: %s', file.size)
-    log.info('file.file.path: %s', file.file.path)
-    if file.mime in ['image/jpe', 'image/jpg', 'image/jpeg', 'image/webp']:
-        processor = ImageProcessor(file, file.file.path)
-        processor.process_file()
-    file.save()
+    # we want to use a temporary local file to support cloud storage cases
+    # this allows us to modify the file before upload
+    file = Files(user=user, **kwargs)
+    with tempfile.NamedTemporaryFile(suffix=os.path.basename(name)) as fp:
+        fp.write(f.read())
+        print(fp.name)
+        file_mime, _ = mimetypes.guess_type(name, strict=False)
+        if not file_mime:
+            file_mime, _ = mimetypes.guess_type(name, strict=False)
+        file_mime = file_mime or 'application/octet-stream'
+        if file_mime in ['image/jpe', 'image/jpg', 'image/jpeg', 'image/webp']:
+            processor = ImageProcessor(fp.name, user.remove_exif, user.remove_exif_geo)
+            processor.process_file()
+            file.meta = processor.meta
+            file.exif = processor.exif
+        file.file = File(fp, name=name)
+        file.name = os.path.basename(file.file.name)
+        log.info('file.name: %s', file.name)
+        file.mime = file_mime
+        log.info('file.mime: %s', file.mime)
+        file.size = file.file.size
+        log.info('file.size: %s', file.size)
+        file.save()
     send_discord_message.delay(file.pk)
-    # TODO: Why are we not just using django.conf.settings to check this?
-    if use_s3():
-        # TODO: This should probably async
-        os.remove(file.file.path)
     return file
