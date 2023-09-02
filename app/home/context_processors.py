b3a29de1f42b716e0bc012b56c7bf4cde565cb00
Ralph Luaces <ralph@luac.es>
2023-09-01 22:46:48 -0500

    fix tests

--- app/home/context_processors.py
+++ app/home/context_processors.py
@@ -5,6 +5,6 @@ from django.forms.models import model_to_dict
 
 def site_settings(request):
     if (site_settings := cache.get('site_settings')) is None:
-        site_settings = SiteSettings.objects.get(pk=1)
+        site_settings, _ = SiteSettings.objects.get_or_create(pk=1)
         cache.set('site_settings', model_to_dict(site_settings))
     return {'site_settings': site_settings}
