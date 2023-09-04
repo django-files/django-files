2f522528fe4423998ebd47d7bffa49b89ceac75e
Shane <6071159+smashedr@users.noreply.github.com>
2023-09-03 22:50:27 -0700

    Minor Fixes

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,23 +1,25 @@
 $(document).ready(function () {
     // Back to Top Button, Function, and Listener
     let mybutton = document.getElementById('btn-back-to-top')
-    window.onscroll = function () {
-        scrollFunction()
-    }
-    function scrollFunction() {
-        if (
-            document.body.scrollTop > 20 ||
-            document.documentElement.scrollTop > 20
-        ) {
-            mybutton.style.display = 'block'
-        } else {
-            mybutton.style.display = 'none'
+    if (mybutton) {
+        window.onscroll = function () {
+            scrollFunction()
         }
-    }
-    mybutton.addEventListener('click', backToTop)
-    function backToTop() {
-        document.body.scrollTop = 0
-        document.documentElement.scrollTop = 0
+        function scrollFunction() {
+            if (
+                document.body.scrollTop > 20 ||
+                document.documentElement.scrollTop > 20
+            ) {
+                mybutton.style.display = 'block'
+            } else {
+                mybutton.style.display = 'none'
+            }
+        }
+        mybutton.addEventListener('click', backToTop)
+        function backToTop() {
+            document.body.scrollTop = 0
+            document.documentElement.scrollTop = 0
+        }
     }
 
     // // Monitor home websockets for new data
