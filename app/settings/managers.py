c4171b994d96448e7923eb1f222b95e9251cba78
Shane <6071159+smashedr@users.noreply.github.com>
2023-09-08 10:46:55 -0700

    codacy

--- app/settings/managers.py
+++ app/settings/managers.py
@@ -3,5 +3,4 @@ from django.db import models
 
 class SiteSettingsManager(models.Manager):
     def settings(self):
-        site_settings, created = self.get_or_create(pk=1)
-        return site_settings
+        return self.get_or_create(pk=1)[0]
