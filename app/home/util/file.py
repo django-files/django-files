1e0ac7886f80bf709e44dc1c3a7651edfa48c213
Ralph Luaces <ralph@luac.es>
2023-09-09 14:57:05 -0500

    more fixes

--- app/home/util/file.py
+++ app/home/util/file.py
@@ -32,7 +32,7 @@ def process_file(name: str, f: IO, user_id: int, **kwargs) -> Files:
     user = CustomUser.objects.get(id=user_id)
     log.info('user: %s', user)
     # process name first
-    name = get_formatted_name(user, f.name, kwargs.pop('name_format', None))
+    name = get_formatted_name(user, name, kwargs.pop('name_format', None))
     # we want to use a temporary local file to support cloud storage cases
     # this allows us to modify the file before upload
     file = Files(user=user, **kwargs)
