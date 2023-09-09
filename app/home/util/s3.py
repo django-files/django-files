14df78b22b90f44022e57e9ffd698a6d997892b2
Ralph Luaces <ralph@luac.es>
2023-09-08 23:12:39 -0500

    fix s3 gallery and custom expires

--- app/home/util/s3.py
+++ app/home/util/s3.py
@@ -1,6 +1,6 @@
 import os
 from django.conf import settings
-from storages.backends.s3boto3 import S3Boto3Storage
+from storages.backends.s3 import S3Storage
 
 
 def use_s3():
@@ -9,11 +9,11 @@ def use_s3():
     return False
 
 
-class S3Bucket(S3Boto3Storage):
+class S3Bucket(S3Storage):
     bucket_name = settings.AWS_STORAGE_BUCKET_NAME
 
     def url(self, name, parameters=None, expire=None, http_method=None):
-        url = super().url(name, parameters=None, expire=None, http_method=None)
+        url = super().url(name, parameters, expire, http_method)
         if settings.AWS_S3_CDN_URL:
             return url.replace(f'https://s3.amazonaws.com/{self.bucket_name}', settings.AWS_S3_CDN_URL)
         return url
