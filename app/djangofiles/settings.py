8ada1fbc0cfb40f347b3b7a0ea8a5979b241bbea
Ralph Luaces <ralph@luac.es>
2023-09-10 20:12:29 -0500

    fix secret_key logic

--- app/djangofiles/settings.py
+++ app/djangofiles/settings.py
@@ -28,16 +28,6 @@ print(f'database_type: {database_type}')
 db_location = config('DATABSE_LOCATION', '/data/media/db/database.sqlite3')
 print(f'db_location: {db_location}')
 
-
-if config('TEST', "False") == "True":
-    SECRET_KEY_FROM_FILE = "testsecret"
-else:
-    # we load the file secret key as we use this as a shared secret with nginx for signing
-    with open('/data/media/db/secret.key') as f:
-        print("Loading secretkey from file.")
-        SECRET_KEY_FROM_FILE = f.read().strip()
-
-
 if config('SECRET', None) or config('SECRET_KEY', None):
     # ensure SECRET/SECRET_KEY is exactly 50 characters long
     secret_key = config('SECRET', None) or config('SECRET_KEY')
@@ -48,7 +38,9 @@ if config('SECRET', None) or config('SECRET_KEY', None):
         SECRET_KEY = secret_key[:50]
     print(f'Using SECRET_KEY from ENV: {SECRET_KEY}')
 else:
-    SECRET_KEY = SECRET_KEY_FROM_FILE
+    with open('/data/media/db/secret.key') as f:
+        print("Loading secretkey from file.")
+        SECRET_KEY = f.read().strip()
     print(f'Using SECRET_KEY from FILE: {SECRET_KEY}')
 
 DEBUG = config('DEBUG', 'False', bool)
--- app/docker-entrypoint.sh
+++ app/docker-entrypoint.sh
@@ -3,19 +3,25 @@
 set -ex
 
 
-if [ ! -f "/data/media/db/secret.key" ];then
-    tr -dc 'A-Za-z0-9' < /dev/urandom | head -c 50 > "/data/media/db/secret.key"
-    echo "Created Secret Key File: /data/media/db/secret.key"
-else
-    echo "Using Secret Key File: /data/media/db/secret.key"
+if [ -z "${SECRET}" ] || [ -z "${SECRET_KEY}" ];then
+    if [ ! -f "/data/media/db/secret.key" ];then
+        tr -dc 'A-Za-z0-9' < /dev/urandom | head -c 50 > "/data/media/db/secret.key"
+        echo "Created Secret Key File: /data/media/db/secret.key"
+    else
+        echo "Using Secret Key File: /data/media/db/secret.key"
+    fi
+
+    if echo "${*}" | grep -q "gun\|runserver";then
+
+        python manage.py migrate
+        python manage.py collectstatic --noinput
+        python manage.py appstartup
+
+    fi
+elif [-z "${SECRET}"];
+    SECRET_KEY=$SECRET
 fi
 
-if echo "${*}" | grep -q "gun\|runserver";then
-
-    python manage.py migrate
-    python manage.py collectstatic --noinput
-    python manage.py appstartup
-
-fi
+printf "${SECRET_KEY}" > /data/media/db/secret.key
 
 exec "$@"
