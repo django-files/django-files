883799064b959d24de084f9102ccdd28634b099b
Ralph Luaces <ralph@luac.es>
2023-09-10 20:16:20 -0500

    missed one

--- app/home/util/nginx.py
+++ app/home/util/nginx.py
@@ -10,7 +10,7 @@ def sign_nginx_urls(uri: str) -> str:
     future = datetime.datetime.now() + datetime.timedelta(seconds=settings.STATIC_QUERYSTRING_EXPIRE)
     expiry = calendar.timegm(future.timetuple())
     secure_link = "{uri}{expiry} {key}".format(uri=uri, expiry=expiry,
-                                               key=settings.SECRET_KEY_FROM_FILE).encode(encoding='utf-8')
+                                               key=settings.SECRET_KEY).encode(encoding='utf-8')
     link_hash = hashlib.md5(secure_link).digest()
     base64_hash = base64.urlsafe_b64encode(link_hash)
     str_hash = base64_hash.decode('utf-8').rstrip('=')
