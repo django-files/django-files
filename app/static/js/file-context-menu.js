835db3c140d4c56c45288121c401508b6ac4978f
Ralph Luaces <ralph@luac.es>
2023-09-14 22:45:12 -0500

    fix insecure password generation

--- app/static/js/file-context-menu.js
+++ app/static/js/file-context-menu.js
@@ -145,13 +145,15 @@ $(document).ready(function () {
 
     $('#generatePassword').click(function () {
         // TODO: Cleanup this Listener
-        let chars =
-            '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-        let passwordLength = 12
-        let password = ''
-        for (var i = 0; i <= passwordLength; i++) {
-            var randomNumber = Math.floor(Math.random() * chars.length)
-            password += chars.substring(randomNumber, randomNumber + 1)
+        let chars = '0123456789abcdefghijklmnopqrstuvwxyz!+()ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        let pwordLength = 15;
+        let password = '';
+
+        const array = new Uint32Array(chars.length);
+        window.crypto.getRandomValues(array);
+
+        for (let i = 0; i < pwordLength; i++) {
+        password += chars[array[i] % chars.length];
         }
         $('#password').get(0).value = password
         navigator.clipboard.writeText(password)
