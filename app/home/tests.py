f133e3a388d98951e33829a241dde38145871d43
Shane <6071159+smashedr@users.noreply.github.com>
2023-09-15 18:42:51 -0700

    tweak test

--- app/home/tests.py
+++ app/home/tests.py
@@ -58,7 +58,7 @@ class PlaywrightTest(StaticLiveServerTestCase):
     """Test Playwright"""
     screenshots = 'screenshots'
     # TODO: Add Upload view back
-    views = ['Gallery', 'Files', 'Shorts', 'Stats']
+    views = ['Gallery', 'Upload/Files', 'Upload/Text', 'Files', 'Shorts', 'Stats']
     previews = ['README.md', 'requirements.txt', 'main.html', 'home_tags.py', 'an225.jpg']
     context = None
     browser = None
@@ -148,23 +148,32 @@ class PlaywrightTest(StaticLiveServerTestCase):
 
         for view in self.views:
             print('---------- view: %s' % view)
-            page.locator('.nav-link').locator(f'text={view}').first.click()
+            if '/' in view:
+                log.debug('view: %s', view)
+                menu, view = view.split('/')
+                log.debug('menu: %s', menu)
+                log.debug('view: %s', view)
+                log.debug('NOT IMPLEMENTED!')
+                continue
+            else:
+                page.locator('.nav-link').locator(f'text={view}').first.click()
+
             if view == 'Upload':
-                page.wait_for_timeout(timeout=500)
+                page.wait_for_timeout(timeout=750)
                 self.screenshot(page, view)
             else:
-                page.locator(f'text={view}')
+                # page.locator(f'text={view}')
+                page.get_by_role("heading", name=view)
                 self.screenshot(page, view)
 
             if view == 'Files':
-                self.screenshot(page, f'{view}-DEBUGBEFOREERROR')
                 page.locator('.delete-file-btn').first.click()
                 page.wait_for_timeout(timeout=500)
                 self.screenshot(page, f'{view}-delete-click')
 
                 page.locator('#confirmDeleteFileBtn').click()
-                page.wait_for_timeout(timeout=500)
-                self.screenshot(page, f'{view}-delete-deleted')
+                # page.wait_for_timeout(timeout=500)
+                # self.screenshot(page, f'{view}-delete-deleted')
 
             if view == 'Shorts':
                 page.locator('#url').fill('https://github.com/django-files/django-files/pkgs/container/django-files')
