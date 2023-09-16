d6a487bba80e6b3a385874771b41996d1af282ab
Shane <6071159+smashedr@users.noreply.github.com>
2023-09-15 19:59:25 -0700

    cleanup paste.js

--- app/static/js/paste.js
+++ app/static/js/paste.js
@@ -1,16 +1,12 @@
 $(document).ready(function () {
-    // Get and set the csrf_token
-    const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value
-
     // Handle paste submissions
     $('#sendPaste').submit(function (event) {
         event.preventDefault()
-        // let data = $('#paste-data').val()
-        let formData = new FormData($(this)[0])
-        formData.forEach((value, key) => (formData[key] = value))
-        formData['method'] = 'paste-text'
-        let data = JSON.stringify(formData)
-        console.log(`data: ${data}`)
-        socket.send(data)
+        const formData = new FormData(this)
+        formData.set('method', 'paste-text')
+        const data = Object.fromEntries(formData.entries())
+        const jsonData = JSON.stringify(data)
+        console.log(`data: ${jsonData}`)
+        socket.send(jsonData)
     })
 })
