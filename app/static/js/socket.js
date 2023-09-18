fb51c4d07ca5e21a605c386497a1a31807edea69
Shane <6071159+smashedr@users.noreply.github.com>
2023-09-17 20:09:51 -0700

    socket.js update

--- app/static/js/socket.js
+++ app/static/js/socket.js
@@ -1,38 +1,34 @@
-console.log('Connecting to WebSocket...')
+// WebSocket JS
 
-let ws
+console.log('Connecting to WebSocket...')
 
 const socket = connect()
 
 function connect() {
-    if (location.protocol == 'https:') {
-        ws = new WebSocket('wss://' + window.location.host + '/ws/home/');
-    } else {
-        // TODO: guard against this happening outside of dev
-        ws = new WebSocket('ws://' + window.location.host + '/ws/home/');
+    const protocol = location.protocol === 'https:' ? 'wss:' : 'ws:'
+    const ws = new WebSocket(`${protocol}//${window.location.host}/ws/home/`)
+
+    ws.onopen = function () {
+        console.log('Socket Open')
     }
-    // ws.onopen = function() {
-    //   // subscribe to some channels
-    //   ws.send(JSON.stringify({
-    //       //.... some message the I must send when I connect ....
-    //   }));
-    // };
-  
-    ws.onmessage = function(e) {
-      console.log('Message:', e.data);
-    };
-  
-    ws.onclose = function(e) {
-      console.log('Socket is closed. Reconnect will be attempted in 1 second.', e.reason);
-      setTimeout(function() {
-        connect();
-      }, 1000);
-    };
-  
-    ws.onerror = function(err) {
-      console.error('Socket encountered error: ', err.message, 'Closing socket');
-      ws.close();
-    };
+
+    ws.onmessage = function (event) {
+        console.log(`Socket Message: ${event.data}`)
+    }
+
+    ws.onclose = function (event) {
+        console.log(`Socket Close: ${event.reason}`)
+        setTimeout(function () {
+            console.log('Reconnecting...')
+            connect()
+        }, 15000)
+    }
+
+    ws.onerror = function (event) {
+        console.error('Socket Error')
+        console.log(event)
+        // ws.close()
+    }
 
     return ws
-  }
+}
