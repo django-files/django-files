3ce8fcf4393279d1c6cc17ccaaf870194fa51cf5
raluaces <raluaces@gmail.com>
2023-12-03 18:02:07 -0600

    some fixes

--- app/static/js/preview.js
+++ app/static/js/preview.js
@@ -1,30 +1,33 @@
 // JS for embed/preview.html
 
-document.addEventListener('DOMContentLoaded', domLoaded)
+document.addEventListener('DOMContentLoaded', domLoaded);
 
-document.getElementById('openSidebar').addEventListener('click', openSidebar)
-document.getElementById('closeSidebar').addEventListener('click', closeSidebar)
+document.getElementById('openSidebar').addEventListener('click', openSidebar);
+document.getElementById('closeSidebar').addEventListener('click', closeSidebar);
 
-const previewSidebar = document.getElementById('previewSidebar')
-const previewSidebarWidth = '350px'
+const previewSidebar = document.getElementById('previewSidebar');
+const previewSidebarWidth = '350px';
 
 function domLoaded() {
     if (Cookies.get('previewSidebar')) {
-        previewSidebar.style.width = previewSidebarWidth
+        previewSidebar.style.width = previewSidebarWidth;
+    } else {
+        $(".sidebar-text").fadeOut(200);
     }
-    $(".card-body").fadeOut(200);
 }
 
 function openSidebar() {
-    previewSidebar.style.width = previewSidebarWidth
-    Cookies.set('previewSidebar', 'enabled')
+    previewSidebar.style.width = previewSidebarWidth;
+    document.getElementById("context-placement").style.right = "355px";
+    Cookies.set('previewSidebar', 'enabled');
     $(".openbtn").hide();
-    $(".card-body").fadeIn(300);
+    $(".sidebar-text").fadeIn(300);
 }
 
 function closeSidebar() {
-    previewSidebar.style.width = '0'
-    Cookies.remove('previewSidebar')
+    previewSidebar.style.width = '0';
+    document.getElementById("context-placement").style.right = "50px";
+    Cookies.remove('previewSidebar');
     $(".openbtn").show();
-    $(".card-body").fadeOut(200);
+    $(".sidebar-text").fadeOut(200);
 }
