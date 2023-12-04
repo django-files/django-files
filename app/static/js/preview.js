56ad14caa58d0a400ef45c121ba69dc7e4ee01d6
raluaces <raluaces@gmail.com>
2023-12-03 19:13:24 -0600

    fix codacy

--- app/static/js/preview.js
+++ app/static/js/preview.js
@@ -1,39 +1,39 @@
 // JS for embed/preview.html
 
-document.addEventListener('DOMContentLoaded', domLoaded);
+document.addEventListener('DOMContentLoaded', domLoaded)
 
-document.getElementById('openSidebar').addEventListener('click', openSidebar);
-document.getElementById('closeSidebar').addEventListener('click', closeSidebar);
+document.getElementById('openSidebar').addEventListener('click', openSidebar)
+document.getElementById('closeSidebar').addEventListener('click', closeSidebar)
 
-const previewSidebar = document.getElementById('previewSidebar');
-const previewSidebarWidth = '360px';
+const previewSidebar = document.getElementById('previewSidebar')
+const previewSidebarWidth = '360px'
 
-const contextPlacement = document.getElementById("context-placement")
+const contextPlacement = document.getElementById('context-placement')
 
 function domLoaded() {
     if (Cookies.get('previewSidebar')) {
-        $(".sidebar-text").fadeOut(200);
+        $('.sidebar-text').fadeOut(200)
     } else {
-        previewSidebar.style.width = previewSidebarWidth;
+        previewSidebar.style.width = previewSidebarWidth
     }
 }
 
 function openSidebar() {
-    previewSidebar.style.width = previewSidebarWidth;
+    previewSidebar.style.width = previewSidebarWidth
     if (contextPlacement) {
-        contextPlacement.style.right = previewSidebarWidth;
+        contextPlacement.style.right = '365px'
     }
-    Cookies.remove('previewSidebar');
-    $(".openbtn").hide();
-    $(".sidebar-text").fadeIn(300);
+    Cookies.remove('previewSidebar')
+    $('.openbtn').hide()
+    $('.sidebar-text').fadeIn(300)
 }
 
 function closeSidebar() {
-    previewSidebar.style.width = '0';
+    previewSidebar.style.width = '0'
     if (contextPlacement) {
-        contextPlacement.style.right = "60px";
+        contextPlacement.style.right = '60px'
     }
-    Cookies.set('previewSidebar', 'enabled');
-    $(".openbtn").show();
-    $(".sidebar-text").fadeOut(200);
+    Cookies.set('previewSidebar', 'enabled')
+    $('.openbtn').show()
+    $('.sidebar-text').fadeOut(200)
 }
