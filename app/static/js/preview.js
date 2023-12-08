4c2a0d47e124570193fb82cb185b243c82ea0b99
Ralph Luaces <raluaces@gmail.com>
2023-12-07 22:02:58 -0600

    Preview page redesign (#175)
    * first pass for preview upgrade
    
    * preview redesign
    
    * some fixes
    
    * preview js file
    
    * For ralph sidebar (#176)
    
    * js-cookie
    
    * Update Variable
    
    * Update IDs
    
    * Simplify Cookie Checking
    
    * Update Variable Name
    
    ---------
    
    Co-authored-by: Shane <6071159+smashedr@users.noreply.github.com>
    
    * some fixes
    
    * fixes for guest
    
    * fix
    
    * more cleanup
    
    * fix test
    
    * fix codacy
    
    * ok try fixing test
    
    * tweak default state
    
    * stupid codacy
    
    * tweaks
    
    * For ralph rush delivery (#177)
    
    * Yea Boi
    
    * Set Expire
    
    * Set Expire
    
    ---------
    
    Co-authored-by: Shane <6071159+smashedr@users.noreply.github.com>
    
    * For ralph sidebar cod (#178)
    
    * Fix DIVs
    
    * Use JQuery Selectors ONLY
    
    * Set No Expand on Smaller than 780
    
    * Add Resize Event
    
    * Fix Horizontal Scrollbar
    
    * Minor Change to Main for Creds
    
    ---------
    
    Co-authored-by: Shane <6071159+smashedr@users.noreply.github.com>
    
    * fix dropdown links, and image stretching
    
    * actually fix image issue
    
    * sidebar placement
    
    * Fix Sidebar Placement: Step 1
    
    * change X
    
    * Revert "change X"
    
    This reverts commit bdc25a44ad04acd474a3d7289cbfdc37bdf01a5a.
    
    * add x, test vh-100
    
    * Fix Sidebar Placement: Step 2
    
    * remove duplciate viewer count
    
    * tweaks
    
    * font fix
    
    * All The Things (#179)
    
    Co-authored-by: Shane <6071159+smashedr@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Shane <6071159+smashedr@users.noreply.github.com>

---
+++ app/static/js/preview.js
@@ -0,0 +1,64 @@
+// JS for embed/preview.html
+
+document.addEventListener('DOMContentLoaded', domLoaded)
+
+window.addEventListener('resize', checkSize)
+
+const previewSidebar = $('#previewSidebar')
+const contextPlacement = $('#contextPlacement')
+const sidebarCard = $('.sidebarCard')
+const openSidebarButton = $('#openSidebar')
+
+openSidebarButton.on('click', openSidebarCallback)
+$('#closeSidebar').on('click', closeSidebarCallback)
+
+const sidebarMaxWidth = 780
+
+function domLoaded() {
+    if (window.innerWidth >= sidebarMaxWidth) {
+        if (!Cookies.get('previewSidebar')) {
+            openSidebar()
+        }
+    }
+}
+
+/**
+ * TODO: Add method to know if side bar is expanded or collapsed
+ */
+function checkSize() {
+    if (window.innerWidth >= sidebarMaxWidth) {
+        if (!Cookies.get('previewSidebar')) {
+            openSidebar()
+        }
+    } else {
+        closeSidebar()
+    }
+}
+
+function openSidebarCallback() {
+    openSidebar()
+    Cookies.remove('previewSidebar')
+}
+
+function closeSidebarCallback() {
+    closeSidebar()
+    Cookies.set('previewSidebar', 'disabled', { expires: 365 })
+}
+
+function openSidebar() {
+    previewSidebar.css('width', '360px')
+    if (contextPlacement) {
+        contextPlacement.css('right', '365px')
+    }
+    openSidebarButton.hide()
+    sidebarCard.fadeIn(300)
+}
+
+function closeSidebar() {
+    previewSidebar.css('width', '0')
+    if (contextPlacement) {
+        contextPlacement.css('right', '60px')
+    }
+    openSidebarButton.show()
+    sidebarCard.fadeOut(200)
+}
