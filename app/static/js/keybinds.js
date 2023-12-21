b19c8d1ccf39d9c423a54fdac581bb4aaa1850bf
Shane <6071159+smashedr@users.noreply.github.com>
2023-12-21 13:33:14 -0800

    Fix Duplication

--- app/static/js/keybinds.js
+++ app/static/js/keybinds.js
@@ -1,34 +1,30 @@
 // JS for Keyboard Shortcuts
 
+const keyLocations = {
+    KeyA: '/uppy/',
+    KeyS: '/settings/user/',
+    KeyD: '/settings/site/',
+    KeyF: '/files/',
+    KeyG: '/gallery/',
+    KeyH: '/',
+    KeyR: '/shorts/',
+    KeyT: '/paste/',
+    KeyY: '/admin/settings/sitesettings/1/change/',
+    KeyX: '/settings/sharex/',
+}
+
 window.addEventListener('keydown', (e) => {
     // console.log('handleKeyboard:', e)
     if (e.altKey || e.ctrlKey || e.metaKey || e.shiftKey || e.repeat) {
         return
     }
-    if (['INPUT', 'TEXTAREA', 'SELECT', 'OPTION'].includes(e.target.tagName)) {
+    const tagNames = ['INPUT', 'TEXTAREA', 'SELECT', 'OPTION']
+    if (tagNames.includes(e.target.tagName)) {
         return
     }
-    if (['KeyZ', 'KeyK'].includes(e.code)) {
+    if (keyLocations[e.code]) {
+        window.location = keyLocations[e.code]
+    } else if (['KeyZ', 'KeyK'].includes(e.code)) {
         $('#keybinds-modal').modal('toggle')
-    } else if (e.code === 'KeyA') {
-        window.location = '/uppy/'
-    } else if (e.code === 'KeyS') {
-        window.location = '/settings/user/'
-    } else if (e.code === 'KeyD') {
-        window.location = '/settings/site/'
-    } else if (e.code === 'KeyF') {
-        window.location = '/files/'
-    } else if (e.code === 'KeyG') {
-        window.location = '/gallery/'
-    } else if (e.code === 'KeyH') {
-        window.location = '/'
-    } else if (e.code === 'KeyR') {
-        window.location = '/shorts/'
-    } else if (e.code === 'KeyT') {
-        window.location = '/paste/'
-    } else if (e.code === 'KeyY') {
-        window.location = '/admin/settings/sitesettings/1/change/'
-    } else if (e.code === 'KeyX') {
-        window.location = '/settings/sharex/'
     }
 })
