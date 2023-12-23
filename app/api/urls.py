4bfc0a2c1ea798995d61366389e65602ff45a89f
Shane <6071159+smashedr@users.noreply.github.com>
2023-12-23 01:05:30 -0800

    API Delete Endpoint

--- app/api/urls.py
+++ app/api/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     path('recent/', views.recent_view, name='recent'),
     path('remote/', views.remote_view, name='remote'),
     path('stats/', views.stats_view, name='stats'),
+    path('delete/<path:idname>', views.delete_view, name='delete'),
 ]
