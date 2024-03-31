b7e2a91c5f0eda350ebbb8047c94570221640835
raluaces <raluaces@gmail.com>
2024-03-31 16:28:23 -0500

    cleanup and flake

--- app/home/managers.py
+++ app/home/managers.py
@@ -4,10 +4,10 @@ from django.db import models
 class FilesManager(models.Manager):
     def get_request(self, request, **kwargs):
         return self.filter(user=request.user, avatar=False, **kwargs)
-    
+
     def get_all_request(self, **kwargs):
         return self.all(avatar=False, **kwargs)
-    
+
     def filtered_request(self, request, **kwargs):
         return self.filter(avatar=False, **kwargs)
 
