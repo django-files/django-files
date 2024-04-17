524c01b918b85c6dfdb1a2720deee58c1951ff2f
Shane <6071159+smashedr@users.noreply.github.com>
2024-04-17 01:15:38 -0700

    Add Some Styling

--- app/static/js/gallery.js
+++ app/static/js/gallery.js
@@ -1,15 +1,16 @@
 // Gallery JS
 
 document.addEventListener('DOMContentLoaded', initGallery)
-window.addEventListener('resize', debounce(galleryScroll, 50))
 document.addEventListener('scroll', debounce(galleryScroll, 50))
+window.addEventListener('resize', debounce(galleryScroll, 50))
 
-const container = document.getElementById('gallery-container')
-const source = document.getElementById('gallery-source')
+const galleryContainer = document.getElementById('gallery-container')
+const imageNode = document.querySelector('div.d-none img')
+
 let nextPage = 1
 
 async function initGallery() {
-    console.log('Init Gallery')
+    console.debug('Init Gallery')
     await addNodes()
 }
 
@@ -17,18 +18,20 @@ async function initGallery() {
  * Gallery onScroll Callback
  * TODO: End of page detection may need to be tweaked/improved
  * @function galleryScroll
+ * @param {Event} event
+ * @param {Number} buffer
  */
-async function galleryScroll() {
-    const buffer = 600
+async function galleryScroll(event, buffer = 600) {
     console.debug(
         `galleryScroll: ${window.scrollY} > ${window.scrollMaxY - buffer}`,
         window.scrollY > window.scrollMaxY - buffer
     )
-    if (nextPage) {
-        if (!window.scrollMaxY || window.scrollY > window.scrollMaxY - buffer) {
-            console.debug('End of Scroll')
-            await addNodes()
-        }
+    if (
+        nextPage &&
+        (!window.scrollMaxY || window.scrollY > window.scrollMaxY - buffer)
+    ) {
+        console.debug('End of Scroll')
+        await addNodes()
     }
 }
 
@@ -37,21 +40,57 @@ async function galleryScroll() {
  * @function addNodes
  */
 async function addNodes() {
-    console.log('addNode')
+    console.debug('addNode')
     if (!nextPage) {
         return console.warn('No Next Page:', nextPage)
     }
     const data = await fetchGallery(nextPage)
+    console.debug('data:', data)
     nextPage = data.next
-    console.log('data:', data)
     for (const file of data.files) {
-        const img = source.querySelector('img').cloneNode(true)
-        img.src = file.thumb
+        // console.debug('file:', file)
+
+        const div = document.createElement('div')
+        div.classList.add(
+            'm-1',
+            'rounded-1',
+            'border',
+            'border-3',
+            'border-secondary',
+            'gallery-div'
+        )
+        div.style.position = 'relative'
+        div.style.overflow = 'hidden'
+        const box1 = '#919191'
+        const box2 = '#495057'
+        div.style.backgroundColor = '#495057'
+        div.style.backgroundImage = `linear-gradient(45deg, ${box2} 25%, transparent 25%), linear-gradient(-45deg, ${box2} 25%, transparent 25%), linear-gradient(45deg, transparent 75%, ${box2} 75%), linear-gradient(-45deg, transparent 75%, ${box2} 75%)`
+        div.style.backgroundImage = `linear-gradient(45deg, ${box1} 25%, transparent 25%), linear-gradient(135deg, ${box1} 25%, transparent 25%), linear-gradient(45deg, transparent 75%, ${box1} 75%), linear-gradient(135deg, transparent 75%, ${box1} 75%)`
+        div.style.backgroundSize = '25px 25px'
+        div.style.position = '0 0, 12.5px 0, 12.5px -12.5px, 0px 12.5px'
+
+        // const img = document.createElement('img')
+        // img.style.maxWidth = '512px'
+        // img.style.maxHeight = '512px'
+        const img = imageNode.cloneNode(true)
+        img.src = file.thumb || file.raw
+
         const link = document.createElement('a')
         link.href = file.url
-        link.appendChild(img)
         link.title = file.name
-        container.appendChild(link)
+
+        const text = document.createElement('div')
+        text.classList.add('text-nowrap', 'small', 'gallery-text')
+        text.textContent = file.name
+        text.style.position = 'absolute'
+        text.style.bottom = '4px'
+        text.style.left = '6px'
+
+        link.appendChild(img)
+        div.appendChild(link)
+        div.appendChild(text)
+        galleryContainer.appendChild(div)
+        // galleryContainer.appendChild(link)
     }
 }
 
