3fa7b4d556d93b9e13de7d2c53f4a684a784fdad
Ralph Luaces <ralph@luac.es>
2024-05-12 14:40:16 -0500

    unified socket handler

--- app/static/js/file-context-menu.js
+++ app/static/js/file-context-menu.js
@@ -11,20 +11,6 @@ $('.ctx-private').on('click', ctxSetPrivate)
 $('.ctx-password').on('click', ctxSetPassword)
 $('.ctx-delete').on('click', ctxDeleteFile)
 
-socket?.addEventListener('message', function (event) {
-    // console.log('socket.message: file-context-menu.js:', event)
-    const data = JSON.parse(event.data)
-    if (data.event === 'set-expr-file') {
-        messageExpire(data)
-    } else if (data.event === 'toggle-private-file') {
-        messagePrivate(data)
-    } else if (data.event === 'set-password-file') {
-        messagePassword(data)
-    } else if (data.event === 'file-delete') {
-        messageDelete(data)
-    }
-})
-
 // Expire Form
 
 fileExpireModal.on('shown.bs.modal', function (event) {
@@ -154,62 +140,6 @@ function getPrimaryKey(event) {
         pk = $(this).parent().parent().parent().data('pk')
     }
     return pk
-}
-
-// Socket Handlers
-
-function messageExpire(data) {
-    console.log('messageExpire:', data)
-    const expireText = $(`#file-${data.id} .expire-value`)
-    const expireIcon = $(`#file-${data.id} .expire-icon`)
-    if (data.expr) {
-        expireText.text(data.expr).data('clipboard-text', data.expr)
-        expireIcon.attr('title', `File Expires in ${data.expr}`).show()
-        show_toast(`${data.name} - Expire set to: ${data.expr}`, 'success')
-    } else {
-        expireText.text('Never').data('clipboard-text', 'Never')
-        expireIcon.attr('title', 'No Expiration').hide()
-        show_toast(`${data.name} - Cleared Expiration.`, 'success')
-    }
-}
-
-function messagePrivate(data) {
-    // TODO: Cleanup Selectors
-    console.log('messagePrivate:', data)
-    const privateStatus = $(`#file-${data.id} .privateStatus`)
-    const previewIcon = $(`#previewIcon`)
-    const ctxPrivateText = $(`#ctx-menu-${data.id} .privateText`)
-    const ctxPrivateIcon = $(`#ctx-menu-${data.id} .privateIcon`)
-    if (data.private) {
-        privateStatus.show()
-        previewIcon.show()
-        ctxPrivateText.text('Make Public')
-        ctxPrivateIcon.removeClass('fa-lock').addClass('fa-lock-open')
-        show_toast(`File ${data.name} set to private.`, 'success')
-    } else {
-        privateStatus.hide()
-        previewIcon.hide()
-        ctxPrivateText.text('Make Private')
-        ctxPrivateIcon.removeClass('fa-lock-open').addClass('fa-lock')
-        show_toast(`File ${data.name} set to public.`, 'success')
-    }
-}
-
-function messagePassword(data) {
-    console.log('messagePassword', data)
-    const passwordStatus = $(`#file-${data.id} .passwordStatus`)
-    if (data.password) {
-        passwordStatus.show()
-        show_toast(`Password set for ${data.name}`, 'success')
-    } else {
-        passwordStatus.hide()
-        show_toast(`Password unset for ${data.name}`, 'success')
-    }
-    filePasswordModal.modal('hide')
-}
-
-function messageDelete(data) {
-    $(`#file-${data.pk}`).remove()
 }
 
 /**
--- app/static/js/user.js
+++ app/static/js/user.js
@@ -2,24 +2,6 @@
 
 const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value
 
-socket?.addEventListener('message', function (event) {
-    console.log('socket.message: user.js:', event)
-    let data = JSON.parse(event.data)
-    // console.log('data:', data)
-    if (data.event === 'file-new') {
-        const message = `New File Upload: ${data.pk}`
-        show_toast(message, 'success')
-    } else if (data.event === 'file-delete') {
-        const message = `File Deleted: ${data.pk}`
-        show_toast(message, 'success')
-    } else if (data.event === 'message') {
-        console.log(`data.message: ${data.message}`)
-        const bsClass = data.bsClass || 'info'
-        const delay = data.delay || '6000'
-        show_toast(data.message, bsClass, delay)
-    }
-})
-
 $('.log-out').on('click', function (event) {
     // console.log('.log-out click', event)
     event.preventDefault()
