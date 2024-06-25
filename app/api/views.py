e60be8bbe78161bb90899bf4de77bb96284317bb
Shane <6071159+smashedr@users.noreply.github.com>
2024-06-25 15:05:36 -0700

    Many Fixes

--- app/api/views.py
+++ app/api/views.py
@@ -463,7 +463,8 @@ def remote_view(request):
 
 def parse_headers(headers: dict, **kwargs) -> dict:
     # TODO: Review This Function
-    allowed = ['format', 'embed', 'password', 'private', 'strip-gps', 'strip-exif', 'auto-password', 'expr', 'avatar', 'albums']
+    allowed = ['format', 'embed', 'password', 'private', 'strip-gps', 'strip-exif', 'auto-password', 'expr', 'avatar',
+               'albums']
     data = {}
     # TODO: IMPORTANT: Determine why these values are not 1:1 - meta_preview:embed
     difference_mapping = {'embed': 'meta_preview'}
