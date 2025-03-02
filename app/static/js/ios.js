f41890cb8431b7a6760c27bd87e95b70f4ea67c1
Ralph Luaces <16159770+raluaces@users.noreply.github.com>
2025-03-02 15:41:05 -0600

    support ios client webview

---
+++ app/static/js/ios.js
@@ -0,0 +1,9 @@
+
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navbarSupportedContent')
+const bsCollapse = new bootstrap.Collapse(menuToggle, {toggle:false})
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => { bsCollapse.toggle() })
+})
+
+document.body.getElementsByClassName("navbar")[0].style.paddingTop = "50px";
