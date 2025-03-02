c5d574aeec52bef73325480af7d88b54d08731be
Ralph Luaces <16159770+raluaces@users.noreply.github.com>
2025-03-02 15:51:04 -0600

    cleanup

--- app/static/js/ios.js
+++
@@ -1,6 +0,0 @@
-// const navLinks = document.querySelectorAll('.nav-item')
-// const menuToggle = document.getElementById('navbarSupportedContent')
-// const bsCollapse = new bootstrap.Collapse(menuToggle, {toggle:false})
-// navLinks.forEach((l) => {
-//     l.addEventListener('click', () => { bsCollapse.toggle() })
-// })
