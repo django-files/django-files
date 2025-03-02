0cfb44b97ce0403727ec36ea24b6a263ad49abcc
Ralph Luaces <16159770+raluaces@users.noreply.github.com>
2025-03-02 15:50:44 -0600

    fixes

--- app/static/js/ios.js
+++ app/static/js/ios.js
@@ -1,9 +1,6 @@
-
-const navLinks = document.querySelectorAll('.nav-item')
-const menuToggle = document.getElementById('navbarSupportedContent')
-const bsCollapse = new bootstrap.Collapse(menuToggle, {toggle:false})
-navLinks.forEach((l) => {
-    l.addEventListener('click', () => { bsCollapse.toggle() })
-})
-
-document.body.getElementsByClassName("navbar")[0].style.paddingTop = "50px";
+// const navLinks = document.querySelectorAll('.nav-item')
+// const menuToggle = document.getElementById('navbarSupportedContent')
+// const bsCollapse = new bootstrap.Collapse(menuToggle, {toggle:false})
+// navLinks.forEach((l) => {
+//     l.addEventListener('click', () => { bsCollapse.toggle() })
+// })
