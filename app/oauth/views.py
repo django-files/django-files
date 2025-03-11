974be3b7788273b15d9c3b8dcdce8584807b5cbe
Ralph Luaces <16159770+raluaces@users.noreply.github.com>
2025-03-10 21:14:20 -0500

    details

--- app/oauth/views.py
+++ app/oauth/views.py
@@ -58,12 +58,12 @@ def oauth_show(request):
     return render(request, "login.html", {"local": site_settings.get_local_auth()})
 
 
-def oauth_methods(request):
+@require_http_methods(["GET"])
+def oauth_methods():
     """
     View     /oauth/methods/
-    Accepts request and returns list of configured methods of oauth.
+    returns list of configured methods of oauth.
     """
-    
     site_settings = SiteSettings.objects.settings() 
     methods = []
     if site_settings.discord_client_id:
@@ -72,7 +72,7 @@ def oauth_methods(request):
         methods.append("github")
     if site_settings.google_client_id:
         methods.append("google")
-    return JsonResponse({"methods": methods})
+    return JsonResponse({"oauth_methods": methods})
 
 
 def oauth_discord(request):
