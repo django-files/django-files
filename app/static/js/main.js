08addea2beda7dee34000fb6bad4c6ecaa1ddf54
Shane <6071159+smashedr@users.noreply.github.com>
2025-05-28 20:38:15 -0700

    Add Mobile Auth Nav Link to tablet

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -84,7 +84,7 @@ function domContentLoaded() {
     const parser = new UAParser()
     const ua = parser.getResult()
     console.log('ua:', ua)
-    if (ua.device?.type === 'mobile') {
+    if (ua.device?.type === 'mobile' || ua.device?.type === 'tablet') {
         console.log(`%cMobile Client Detected`, 'color: Lime')
         const navItem = document.getElementById('mobileAuthNav')
         navItem.classList.remove('d-none')
