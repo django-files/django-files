f3e6702737e0ca6d61a106419d06a7cc22b6017b
Shane <6071159+smashedr@users.noreply.github.com>
2025-08-04 18:46:16 -0700

    #noci Add Viewer Count to Website

--- app/static/js/live.js
+++ app/static/js/live.js
@@ -1,20 +1,25 @@
 // JS for live.html
 
+const viewCountText = document.getElementById('view-count')
+const streamName = window.location.pathname.split('/')[2]
+
+let checkInterval
 let pingInterval
+let player
 
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('DOMContentLoaded: live.js')
-    const player = videojs('my-video', {
+    console.log(`DOMContentLoaded: live.js - ${streamName}`)
+    player = videojs('my-video', {
         // fluid: true,
         fill: true,
         autoplay: 'play',
     })
     console.log('player:', player)
-    // setTimeout(pingServer, 1000 * 10)
-    // setInterval(pingServer, 1000 * 60)
-    // player?.ready(() => {
-    //     console.log('%c player?.ready:', 'color: Lime', player)
-    // })
+    if (!checkInterval) {
+        // noinspection JSIgnoredPromiseFromCall
+        checkStream()
+        checkInterval = setInterval(checkStream, 1000 * 60)
+    }
     player.on('play', () => {
         console.log('%c player.on: play', 'color: Lime')
         // noinspection JSIgnoredPromiseFromCall
@@ -27,18 +32,32 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 async function pingServer() {
-    const player = videojs.getPlayer('my-video')
-    // console.log('player:', player)
-    const name = window.location.pathname.split('/')[2]
-    if (name && player && !player.paused()) {
+    console.log(`pingServer: ${streamName}:`, player)
+    if (streamName && player && !player.paused()) {
         console.log('%c PLAYING:', 'color: Lime')
         if (!pingInterval) {
             console.log('%c setInterval: pingServer', 'color: Lime')
             pingInterval = setInterval(pingServer, 1000 * 58)
         }
-        const response = await fetch(`/api/stream/ping/${name}/`)
+        const response = await fetch(`/api/stream/ping/${streamName}/`)
         console.log('response:', response)
     } else {
-        console.log('%c PAUSED or no player:', 'color: OrangeRed', player)
+        console.log('%c PAUSED or NO player:', 'color: OrangeRed', player)
     }
+}
+
+async function checkStream() {
+    console.log(`checkStream: ${streamName}:`, player)
+    const url = `/api/stream/viewers/${streamName}/`
+    const options = { headers: { Accept: 'application/json' } }
+    fetch(url, options).then((response) => {
+        console.log('response:', response)
+        if (response.ok) {
+            response.json().then((data) => {
+                console.log('data:', data)
+                viewCountText.textContent = data.count
+            })
+        }
+    })
+    // .catch((e) => console.log('checkStream: catch:', e))
 }
