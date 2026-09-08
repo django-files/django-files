45e32075857639a88e101c19dfdf38367095e169
Ralph Luaces <16159770+raluaces@users.noreply.github.com>
2026-09-08 18:45:31 -0500

    Fix paste text upload redirecting to raw API JSON (#439) (#441)
    * Fix paste text upload redirecting to raw API JSON (#439)
    
    * Link to file share page in paste upload toast (#439)

--- app/static/js/paste.js
+++ app/static/js/paste.js
@@ -25,6 +25,16 @@ $('#send-paste').on('submit', function (event) {
         success: function (data) {
             console.log('data:', data)
             form.trigger('reset')
+            const link = $('<span>Text Uploaded: </span>').append(
+                $('<a>', {
+                    href: data.url,
+                    text: data.name,
+                    class: 'link-light',
+                    target: '_blank',
+                    rel: 'noopener',
+                })
+            )
+            show_toast(link, 'success')
         },
         error: messageErrorHandler,
         cache: false,
